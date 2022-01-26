8689cd072d38e5b0d52a21137dfe09ada6bde3f3
PatrykP <patryk.pasciakk@gmail.com>
2022-01-26 06:28:57 +0100

    Error pages
    Error pages for 401, 403, 404

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+<div class="container justify-content-center mt-5">
+  <i class="fas fa-exclamation-triangle" style="font-size:80px; width:100%; text-align:center;"></i>
+  <div style="font-size:60px; width:100%; text-align:center;">401</div>
+  <div style="font-size:30px; width:100%; text-align:center;">Nie masz uprawnień by tu zaglądać :)</div>
+</div>
+@endsection
